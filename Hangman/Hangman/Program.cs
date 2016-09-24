a57a63e697c3023096d82b9690fe322b5028c592
alonsobugs <alonsobugs@gmail.com>
2016-09-24 15:12:00 -0600

    cambio en adivinar letra

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -11,9 +11,44 @@ namespace Hangman
         static void Main(string[] args)
         {
 
-            string Palabra = ;
-            cambio creado;
+            string PalabraAdivinar = ;
+            string PalabaAdivinarUppercase = PalabraAdivinar.ToUpper();
+            char Letra = ;
+            int Intentos = 10;
+            bool gano = false;
+            int LetrasReveladas = 0;
 
+            StringBuilder displayToPlayer = new StringBuilder(PalabraAdivinar.Length);
+            for (int i = 0; i < PalabraAdivinar.Length; i++)
+                displayToPlayer.Append('_');
+
+            List<char> LetrasAdivinadas = new List<char>();
+            List<char> LetrasErroneas = new List<char>();
+
+            string input;
+            char guess;
+
+            while (!gano && Intentos > 0)
+            {
+                Console.Write("adivinar letra: ");
+
+                input = Console.ReadLine().ToUpper();
+                guess = input[0];
+
+                if (LetrasAdivinadas.Contains(guess))
+                {
+                    Console.WriteLine("Ya intento con esta letra '{0}', y era correcta", guess);
+                    continue;
+                }
+                else if (LetrasErroneas.Contains(guess))
+                {
+                    Console.WriteLine(" Ya intento con esta letra '{0}', y era erronea", guess);
+                    continue;
+                }
+
+
+            }
         }
+        
     }
 }
